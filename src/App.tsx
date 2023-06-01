9bcfbb530e1ead9bb10bcb0c46d416eedd5c0251
Taras <baranprivat22@gmail.com>
2023-06-02 00:35:07 +0200

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { Navigate, Route, Routes } from 'react-router';
-import Home from './pages/Home/Home';
-import Layout from './components/Layout/Layout';
-import Top from './pages/Top/Top';
-import Random from './pages/Random/Random';
-import Quiz from './pages/Quiz/Quiz';
-import User from './pages/User/User';
-import Auth from './pages/Auth/Auth';
+import { lazy } from 'react';
+import Country from './pages/Country/Country';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Layout = lazy(() => import('./components/Layout/Layout'));
+const Top = lazy(() => import('./pages/Top/Top'));
+const Random = lazy(() => import('./pages/Random/Random'));
+const Quiz = lazy(() => import('./pages/Quiz/Quiz'));
+const User = lazy(() => import('./pages/User/User'));
+const Auth = lazy(() => import('./pages/Auth/Auth'));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/home" element={<Navigate to="/" replace />} />
-        <Route path="/top" element={<Top />} />
-        <Route path="/random" element={<Random />} />
+        <Route path="/home">
+          <Route index element={<Navigate to="/" replace />} />
+          <Route path=":id" element={<Country />} />
+        </Route>
+        <Route path="/top">
+          <Route index element={<Top />} />
+          <Route path=":id" element={<Country />} />
+        </Route>
+        <Route path="/random">
+          <Route index element={<Random />} />
+          <Route path=":id" element={<Country />} />
+        </Route>
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/user" element={<User />} />
         <Route path="/signUp" element={<Auth />} />

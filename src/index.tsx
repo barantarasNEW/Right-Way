dd69e1cf76d5243ba699e635f47acc2529b688b8
Taras <baranprivat22@gmail.com>
2023-06-02 00:34:58 +0200

    updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,12 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './styles/style.scss';
 import App from './App';
-import Modal from './components/Modal/Modal';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <HashRouter>
     <Provider store={store}>
       <App />
-      <Modal />
     </Provider>
   </HashRouter>
 );

ad088410ab6f43b8db2c5508eee9d6a7e62db912
Taras <baranprivat22@gmail.com>
2023-06-02 23:02:50 +0200

    updated api

--- src/api/getCountry.ts
+++ src/api/getCountry.ts
@@ -1,19 +1,19 @@
-import { countryFields } from "../constants/constants";
+export const FIELDS = "name,capital,population,region,flags,independent,currencies,languages,maps";
 
 const wait = (time: number) => {
   return new Promise(resolve => setTimeout(resolve, time));
 };
 
-export const getCountry = (
-  name: string,
-  fields: string = countryFields
-) => {
+export const getCountry = (name: string) => {
   return wait(300)
-    .then(() => fetch(`${process.env.API_URL}/name/${name}?fields=${fields}`))
+    .then(() => fetch(`${process.env.API_URL}/name/${name}?fields=${FIELDS}`))
     .then(res => res.json())
     .then(res => ({
       ...res[0],
       name: res[0].name.common,
-      flag: res[0].flags.png
+      flag: res[0].flags.png,
+      currencies: Object.values(res[0].currencies),
+      languages: Object.values(res[0].languages),
+      map: res[0].maps.googleMaps,
     }));
 };
